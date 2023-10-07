b888f20a8f127b3c521aef093378b8a3f9051b46
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-10-07 20:45:26 -0300

    se agrega css, se cambian funcionalidades de presentacion de img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Imgmemes from './componentes/Imgmemes';
-
+import './App.css';
 
 function App() {
   return (
--- src/componentes/Imgmemes.jsx
+++ src/componentes/Imgmemes.jsx
@@ -11,8 +11,8 @@ const Imgmemes = () => {
     setTextmeme(e.target.value);
   }
 
-  const seleccionarImg = (e) => {
-    const memeSeleccionado = memes.find(meme => meme.id === e.target.value);
+  const seleccionarImg = (id) => {
+    const memeSeleccionado = memes.find(meme => meme.id === id);
     if (memeSeleccionado) {
       setImgmeme(memeSeleccionado.url);
     }
@@ -52,22 +52,31 @@ const Imgmemes = () => {
   }, []);
 
   return (
-    <div className="text-center">
+    <div className="meme-editor text-center">
       <h1 className="mt-3 mb-3 text-center">Editor de Memes</h1>
-      <h3>Ingresa el texto del meme</h3>
-      <input onChange={textomeme} className="form-control w-50 m-auto d-block" type="text" placeholder="Ingresa tu frase" name="meme" arial-label="default input example"/>
 
-      <h3 className="mt-3 mb-3 tect-center">Elegí tu imagen favorita</h3>
-      <select onChange={seleccionarImg} className="form-select form-select-lg mb-3 w-50 m-auto" aria-label=".form-select-lg example">
-      {memes.map(meme => (
-          <option key={meme.id} value={meme.id}>{meme.name}</option>
-        ))}
-      </select>      
-
+      <div className="container">
+      {imgmeme && (
       <figure className="text-center" id="exportar">
-        <p className="w-100 px-30 position-absolute top-50 start-30 h1 text-center">{textmeme}</p>
+        <p className="mt-5 w-100 px-30 position-absolute top-50 start-30 h1 text-center">{textmeme}</p>
         <img src={imgmeme} className="figure-img mt-3 d-block m-auto" alt="meme"/>
       </figure>
+      )}
+    
+        <div>
+        <h3>Ingresa el texto del meme</h3>
+        <input onChange={textomeme} className="form-control w-50 m-auto d-block" type="text" placeholder="Ingresa tu frase" name="meme" arial-label="default input example"/>
+        </div>
+      </div>
+
+      <h3 className="mt-3 mb-3 tect-center">Elegí tu imagen favorita</h3>
+      <section className="grid-container">
+      {memes.map(meme => (
+        <div key={meme.id} onClick={() => seleccionarImg(meme.id)}>
+        <img src={meme.url} alt={meme.name} />
+      </div>
+      ))}
+      </section>
 
       <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
     </div>

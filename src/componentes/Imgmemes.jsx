e600bda5ae9c5a7216b8347b2208618ab99a0606
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-10-08 23:37:58 -0300

    se agregan nuevas funciones de edicion

--- src/componentes/Imgmemes.jsx
+++ src/componentes/Imgmemes.jsx
@@ -3,25 +3,34 @@ import React, {useState, useEffect} from "react";
 
 const Imgmemes = () => {
 
-  const [imgmeme, setImgmeme] = useState();
+  const [imgmeme, setImgmeme] = useState('https://i.imgflip.com/2wifvo.jpg');
   const [textmeme, setTextmeme] = useState();
+  const [textmeme2, setTextmeme2] = useState();
   const [memes, setMemes] = useState([]);
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
 
   const textomeme = (e) => {
     setTextmeme(e.target.value);
+  }
+
+  const textomeme2 = (e) => {
+    setTextmeme2(e.target.value);
   }
 
   const seleccionarImg = (id) => {
     const memeSeleccionado = memes.find(meme => meme.id === id);
     if (memeSeleccionado) {
       setImgmeme(memeSeleccionado.url);
+      window.scrollTo(0, 0);
     }
   }
 
   const descargar = (e) => {
-    html2canvas(document.querySelector('#exportar')).then(function(canvas) {
+    html2canvas(document.querySelector('#exportar'), {useCORS: true}).then(function(canvas) {
       /*{document.body.appendChild(canvas);}*/
-      let img = canvas.toDataURL('memes/jpg');
+      let img = canvas.toDataURL('image/jpg');
       let link =  document.createElement('a');
       link.download = "memepropio.jpg";
       link.href = img;
@@ -38,7 +47,8 @@ const Imgmemes = () => {
 
         if(respuesta.status === 200){
           const datos = await respuesta.json();
-          setMemes(datos.data.memes);
+          const memesFiltrados = datos.data.memes.filter(meme => meme.height < 1400);
+          setMemes(memesFiltrados);
         } else {
           console.log("Hubo un error al obtener las imágenes");
         }
@@ -53,21 +63,45 @@ const Imgmemes = () => {
 
   return (
     <div className="meme-editor text-center">
-      <h1 className="mt-3 mb-3 text-center">Editor de Memes</h1>
+      <h1 className="titulo text-center">Editor de Memes</h1>
 
       <div className="container">
-      {imgmeme && (
-      <figure className="text-center" id="exportar">
-        <p className="mt-5 w-100 px-30 position-absolute top-50 start-30 h1 text-center">{textmeme}</p>
-        <img src={imgmeme} className="figure-img mt-3 d-block m-auto" alt="meme"/>
-      </figure>
-      )}
+        {imgmeme && (
+        <figure className="contenedor-meme" id="exportar" >
+          <p className="texto-meme" style={{color: `rgb(${red}, ${green}, ${blue})`}}>{textmeme}</p>
+          <p className="texto-meme2"style={{color: `rgb(${red}, ${green}, ${blue})`}}>{textmeme2}</p>
+          <img src={imgmeme} className="figure-img d-block m-auto" alt="meme"/>
+        </figure>
+        )}
     
-        <div>
-        <h3>Ingresa el texto del meme</h3>
-        <input onChange={textomeme} className="form-control w-50 m-auto d-block" type="text" placeholder="Ingresa tu frase" name="meme" arial-label="default input example"/>
+        <div className="edit-container">
+          <div className="texto">
+            <h3>Ingresa el texto del meme</h3>
+            <div className="input-texto">
+              <input onChange={textomeme} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu primer frase" name="meme" arial-labe="default input example"/>
+
+              <input onChange={textomeme2} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu segunda frase" name="meme" aria-label="default input example"/>
+            </div>
+          </div>
+          
+        <div className="color">
+          <h3>Elige el color de letra</h3>
+          <div className="input-color">
+            <label>R: 
+              <input type="range" min="0" max="255" value={red} onChange={(e) => setRed(e.target.value)}/>
+            </label>
+            <label>G: 
+              <input type="range" min="0" max="255" value={green} onChange={(e) => setGreen(e.target.value)} />
+            </label>
+            <label>B: 
+              <input type="range" min="0" max="255" value={blue} onChange={(e) => setBlue(e.target.value)} />
+            </label>
+          </div>
         </div>
       </div>
+    </div>
+
+      <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
 
       <h3 className="mt-3 mb-3 tect-center">Elegí tu imagen favorita</h3>
       <section className="grid-container">
@@ -77,8 +111,6 @@ const Imgmemes = () => {
       </div>
       ))}
       </section>
-
-      <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
     </div>
   )
 }

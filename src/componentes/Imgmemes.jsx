4f86793919d6d6de9eec4c4fc8922f71a1e429bd
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-10-10 16:55:54 -0300

    se agrega funcion de mover texto

--- src/componentes/Imgmemes.jsx
+++ src/componentes/Imgmemes.jsx
@@ -1,8 +1,7 @@
-import html2canvas from "html2canvas"
+import html2canvas from "html2canvas";
 import React, {useState, useEffect} from "react";
 
 const Imgmemes = () => {
-
   const [imgmeme, setImgmeme] = useState('https://i.imgflip.com/2wifvo.jpg');
   const [textmeme, setTextmeme] = useState();
   const [textmeme2, setTextmeme2] = useState();
@@ -10,6 +9,10 @@ const Imgmemes = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  const [ejex, setEjex] = useState(0);
+  const [ejey, setEjey] =useState(0);
+  const [ejex2, setEjex2] = useState(0);
+  const [ejey2, setEjey2] =useState(0);
 
   const textomeme = (e) => {
     setTextmeme(e.target.value);
@@ -29,14 +32,14 @@ const Imgmemes = () => {
 
   const descargar = (e) => {
     html2canvas(document.querySelector('#exportar'), {useCORS: true}).then(function(canvas) {
-      /*{document.body.appendChild(canvas);}*/
+       /*{document.body.appendChild(canvas);}*/
       let img = canvas.toDataURL('image/jpg');
-      let link =  document.createElement('a');
+      let link = document.createElement('a');
       link.download = "memepropio.jpg";
       link.href = img;
       link.click();
     });
-  } 
+  }
 
   useEffect(() => {
     const cargarImagenes = async() => {
@@ -57,36 +60,31 @@ const Imgmemes = () => {
         console.log(error.message);
       }
     }
-
     cargarImagenes();
   }, []);
 
-  return (
-    <div className="meme-editor text-center">
-      <h1 className="titulo text-center">Editor de Memes</h1>
-
-      <div className="container">
-        {imgmeme && (
+return (
+  <div className="meme-editor text-center">
+    <h1 className="titulo text-center">Editor de Memes</h1>
+    <div className="container">
+      {imgmeme && (
         <figure className="contenedor-meme" id="exportar" >
-          <p className="texto-meme" style={{color: `rgb(${red}, ${green}, ${blue})`}}>{textmeme}</p>
-          <p className="texto-meme2"style={{color: `rgb(${red}, ${green}, ${blue})`}}>{textmeme2}</p>
+          <p className="texto-meme" style={{color: `rgb(${red}, ${green}, ${blue})`, transform: `translate(${ejex}px, ${ejey}px)`}}>{textmeme}</p>
+          <p className="texto-meme2"style={{color: `rgb(${red}, ${green}, ${blue})`, transform: `translate(${ejex2}px, ${255-ejey2}px)`}}>{textmeme2}</p>
           <img src={imgmeme} className="figure-img d-block m-auto" alt="meme"/>
         </figure>
-        )}
-    
-        <div className="edit-container">
-          <div className="texto">
-            <h3>Ingresa el texto del meme</h3>
-            <div className="input-texto">
-              <input onChange={textomeme} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu primer frase" name="meme" arial-labe="default input example"/>
-
-              <input onChange={textomeme2} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu segunda frase" name="meme" aria-label="default input example"/>
-            </div>
+      )}
+      <div className="edit-container">
+        <div className="texto">
+          <h3>Ingresa el texto del meme</h3>
+          <div className="input-texto">
+            <input onChange={textomeme} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu primer texto" name="meme" arial-labe="default input example"/>
+            <input onChange={textomeme2} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu segundo texto" name="meme" aria-label="default input example"/>
           </div>
-          
+        </div>
         <div className="color">
           <h3>Elige el color de letra</h3>
-          <div className="input-color">
+          <div className="input-color"style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}}>
             <label>R: 
               <input type="range" min="0" max="255" value={red} onChange={(e) => setRed(e.target.value)}/>
             </label>
@@ -98,21 +96,47 @@ const Imgmemes = () => {
             </label>
           </div>
         </div>
+        <div>
+          <h3>Posiciona el texto</h3>
+          <div className="position">
+            <div className="position-inputs">
+              <div className="position-input">
+                <h4>Texto 1</h4>
+                <label>horizontal: 
+                  <input type="range" min="0" max="255" value={ejex} onChange={(e) => setEjex(e.target.value)}/>
+                </label>
+                <label>vertical: 
+                  <input type="range" min="0" max="255" value={ejey} onChange={(e) => setEjey(e.target.value)} />
+                </label>
+              </div>
+              <div className="position-input">
+                <h4>Texto 2</h4>
+                <label>horizontal: 
+                  <input type="range" min="0" max="255" value={ejex2} onChange={(e) => setEjex2(e.target.value)}/>
+                </label>
+                <label>vertical: 
+                  <input type="range" min="0" max="255" value={ejey2} onChange={(e) => setEjey2(e.target.value)}/>
+                </label>
+              </div>
+            </div>  
+          </div>
+        </div>
       </div>
     </div>
 
-      <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
+    <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
 
-      <h3 className="mt-3 mb-3 tect-center">Elegí tu imagen favorita</h3>
-      <section className="grid-container">
+    <h3 className="mt-3 mb-3 tect-center">Elegí tu imagen favorita</h3>
+
+    <section className="grid-container">
       {memes.map(meme => (
         <div key={meme.id} onClick={() => seleccionarImg(meme.id)}>
-        <img src={meme.url} alt={meme.name} />
-      </div>
+          <img src={meme.url} alt={meme.name} />
+        </div>
       ))}
-      </section>
-    </div>
-  )
-}
+    </section>
 
+  </div>
+)
+}
 export default Imgmemes;

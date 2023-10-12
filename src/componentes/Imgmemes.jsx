6719834017250d9a20b419b9af17db2fc8e5c42e
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-10-12 18:49:34 -0300

    se agrega funcion cambiar tamaño de letra

--- src/componentes/Imgmemes.jsx
+++ src/componentes/Imgmemes.jsx
@@ -13,6 +13,7 @@ const Imgmemes = () => {
   const [ejey, setEjey] =useState(0);
   const [ejex2, setEjex2] = useState(0);
   const [ejey2, setEjey2] =useState(0);
+  const [fontSize, setFontSize] = useState(40);
 
   const textomeme = (e) => {
     setTextmeme(e.target.value);
@@ -66,65 +67,76 @@ const Imgmemes = () => {
 return (
   <div className="meme-editor text-center">
     <h1 className="titulo text-center">Editor de Memes</h1>
-    <div className="container">
-      {imgmeme && (
-        <figure className="contenedor-meme" id="exportar" >
-          <p className="texto-meme" style={{color: `rgb(${red}, ${green}, ${blue})`, transform: `translate(${ejex}px, ${ejey}px)`}}>{textmeme}</p>
-          <p className="texto-meme2"style={{color: `rgb(${red}, ${green}, ${blue})`, transform: `translate(${ejex2}px, ${255-ejey2}px)`}}>{textmeme2}</p>
-          <img src={imgmeme} className="figure-img d-block m-auto" alt="meme"/>
-        </figure>
-      )}
+    <div className="contenedor">
+    <div id="exportar" style={{display: 'inline-block'}}>
+        {imgmeme && (
+          <figure className="contenedor-meme">
+            <p className="texto-meme" style={{fontSize: `${fontSize}px`,color: `rgb(${red}, ${green}, ${blue})`,  transform: `translate(${ejex}px, ${ejey}px)`}}>{textmeme}</p>
+            <p className="texto-meme2"style={{fontSize: `${fontSize}px`,color: `rgb(${red}, ${green}, ${blue})`, transform: `translate(${ejex2}px, ${ejey2}px)`}}>{textmeme2}</p>
+            <img src={imgmeme} className="figure-img d-block m-auto" alt="meme"/>
+          </figure>
+          )}
+        </div>
+      
       <div className="edit-container">
         <div className="texto">
           <h3>Ingresa el texto del meme</h3>
           <div className="input-texto">
-            <input onChange={textomeme} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu primer texto" name="meme" arial-labe="default input example"/>
-            <input onChange={textomeme2} className="form-control w-50 d-block" type="text" placeholder="Ingresa tu segundo texto" name="meme" aria-label="default input example"/>
+            <input onChange={textomeme} className="form-control d-block" type="text" placeholder="Ingresa tu primer texto" name="meme" arial-labe="default input example"/>
+            <input onChange={textomeme2} className="form-control d-block" type="text" placeholder="Ingresa tu segundo texto" name="meme" aria-label="default input example"/>
           </div>
         </div>
-        <div className="color">
-          <h3>Elige el color de letra</h3>
-          <div className="input-color"style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}}>
-            <label>R: 
-              <input type="range" min="0" max="255" value={red} onChange={(e) => setRed(e.target.value)}/>
-            </label>
-            <label>G: 
-              <input type="range" min="0" max="255" value={green} onChange={(e) => setGreen(e.target.value)} />
-            </label>
-            <label>B: 
-              <input type="range" min="0" max="255" value={blue} onChange={(e) => setBlue(e.target.value)} />
-            </label>
-          </div>
-        </div>
-        <div>
+
+        <div> 
           <h3>Posiciona el texto</h3>
           <div className="position">
             <div className="position-inputs">
               <div className="position-input">
                 <h4>Texto 1</h4>
                 <label>horizontal: 
-                  <input type="range" min="0" max="255" value={ejex} onChange={(e) => setEjex(e.target.value)}/>
+                  <input type="range" min="0" max="800" value={ejex} onChange={(e) => setEjex(e.target.value)}/>
                 </label>
                 <label>vertical: 
-                  <input type="range" min="0" max="255" value={ejey} onChange={(e) => setEjey(e.target.value)} />
+                  <input type="range" min="0" max="800" value={ejey} onChange={(e) => setEjey(e.target.value)} />
                 </label>
               </div>
               <div className="position-input">
                 <h4>Texto 2</h4>
                 <label>horizontal: 
-                  <input type="range" min="0" max="255" value={ejex2} onChange={(e) => setEjex2(e.target.value)}/>
+                  <input type="range" min="0" max="800" value={ejex2} onChange={(e) => setEjex2(e.target.value)}/>
                 </label>
                 <label>vertical: 
-                  <input type="range" min="0" max="255" value={ejey2} onChange={(e) => setEjey2(e.target.value)}/>
+                  <input type="range" min="0" max="800" value={ejey2} onChange={(e) => setEjey2(e.target.value)}/>
                 </label>
               </div>
             </div>  
           </div>
         </div>
-      </div>
     </div>
 
-    <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4">Descargar meme</button>
+    <div className="edit-container2">
+    <div className="tamaño-letra">
+      <label><h3>Tamaño de la letra:</h3></label>
+      <input type="range" min="10" max="100" value={fontSize} onChange={(e) => setFontSize(e.target.value)} />
+    </div>
+    <div className="color">
+            <h3>Elige el color</h3>
+            <div className="input-color"style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}}>
+              <label>R: 
+                <input type="range" min="0" max="255" value={red} onChange={(e) => setRed(e.target.value)}/>
+              </label>
+              <label>G: 
+                <input type="range" min="0" max="255" value={green} onChange={(e) => setGreen(e.target.value)} />
+              </label>
+              <label>B: 
+                <input type="range" min="0" max="255" value={blue} onChange={(e) => setBlue(e.target.value)} />
+              </label>
+            </div>
+        </div>
+    </div>
+      </div>
+
+    <button onClick={descargar} type="button" className="btn btn-primary mt-4 mb-4 ">Descargar meme</button>
 
     <h3 className="mt-3 mb-3 tect-center">Elegí tu imagen favorita</h3>
 
